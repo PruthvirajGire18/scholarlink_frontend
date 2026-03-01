8ef6459db843b3e55b8374adaa48427224702395
PruthvirajGire18 <girepruthviraj@gmail.com>
2026-03-01 09:29:19 +0530

    ui ux improved and lang error solved

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,20 +1,28 @@
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "../contexts/AuthContext";
-import { LanguageProvider } from "../i18n";
+import { LanguageProvider, useTranslation } from "../i18n";
 import { AccessibilityProvider } from "../contexts/AccessibilityContext";
 import AppRoutes from "./routes";
 import Layout from "../components/Layout";
+
+function LocalizedAppShell() {
+  const { lang } = useTranslation();
+
+  return (
+    <AccessibilityProvider>
+      <Layout key={`layout-${lang}`}>
+        <AppRoutes />
+      </Layout>
+    </AccessibilityProvider>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <LanguageProvider>
-          <AccessibilityProvider>
-            <Layout>
-              <AppRoutes />
-            </Layout>
-          </AccessibilityProvider>
+          <LocalizedAppShell />
         </LanguageProvider>
       </AuthProvider>
     </BrowserRouter>
